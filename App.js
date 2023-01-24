1d3786bb5a48b75b1b97da503c4c7c965b30a4d6
Roosep <roopes16@hotmail.com>
2023-01-24 16:50:04 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
+  const buttonPressed = () => {
+    Alert.alert("Button pressed");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+      <Button onPress={buttonPressed} title="Press me" />
     </View>
   );
 }
